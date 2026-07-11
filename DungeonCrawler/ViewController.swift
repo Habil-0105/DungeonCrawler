ff8b000b13e5e7ddc6c1367e130d843a1cb7541f
Habil <habil0105@gmail.com>
2026-07-11 16:41:33 +0700

    initial setup

--- DungeonCrawler/ViewController.swift
+++ DungeonCrawler/ViewController.swift
@@ -17,17 +17,12 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .resizeFill
+            
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
